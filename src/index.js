796059f2a8cbde92886ecffe2094e33a64554611
fdiengott <freddiengott@gmail.com>
2021-06-01 16:02:56 -0400

    Setup index to grab user-inputted text

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-import "./styles/index.scss";
+require("babel-core/register");
+require("babel-polyfill");
+import './styles/index.scss'; 
 
-function startDOM() {
+import { getWordAudio, getWordSyllables } from './scripts/dictionary'; 
+import { setupTracks } from './scripts/track';
+import { playWord, playIPA } from './scripts/audio'; 
+
+const currentStateObj = {
+  currentInput: null,
+  // currentEventListeners: [],
+};
+
+
+// should maybe change this to a debouncing function that fires after a certain amount of time elapses without typing
+document.querySelector("#input-text-form").addEventListener("submit", handleInput);
+
+async function handleInput(e) {
+  e.preventDefault(); 
+  resetTracks(); 
+  
+  const inputText = e.currentTarget.firstElementChild.firstElementChild.value; 
+  const syllables = await getWordSyllables(inputText); 
+  // playWord(inputText); 
+
+  // setupTracks(syllables, document.getElementById('track-wrapper')); 
+  playIPA(syllables[0]); 
+
 }
 
-function startCanvas() {
+
+function startDOM() {
 }
 
 function unregisterEventListeners() { 
 }
 
-function clearDemo() {
+function resetTracks() {
+  console.log("in reset tracks");
 }
