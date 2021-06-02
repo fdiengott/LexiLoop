e075e52307043e5777a1e2d78db0d39d38804bbe
fdiengott <freddiengott@gmail.com>
2021-06-02 16:04:05 -0400

    Update audio to store currentStateObj and add several scheduling methods for playback

--- src/index.js
+++ src/index.js
@@ -3,42 +3,78 @@ require("babel-polyfill");
 import './styles/index.scss'; 
 
 import { getWordAudio, getWordSyllables, syllableToIpa, getWordIPA } from './scripts/dictionary'; 
-import { setupTracks } from './scripts/track';
-import { playWord, playIPA, loadSyllableSound } from './scripts/audio'; 
+import { setupTracks, removeTracks } from './scripts/track';
+import { playWord, playIPA, loadSyllableSound, playSyllable, currentStateObj } from './scripts/audio'; 
 
-const currentStateObj = {
-  currentInput: null,
-  // currentEventListeners: [],
-};
+
+currentStateObj.currentInput = null; 
+
+// {
+//   currentInput: null,
+//   audioContext: null, 
+//   isPlaying: false, 
+//   startTime: null, 
+//   current16thNote: null,     // last note that was scheduled to be played
+//   tempo: 50.0,              // measured in bpm
+//   lookAhead: 25.0,          // how frequently to call the scheduling function
+//   scheduleAheadTime: 0.1,   // how far ahead to schedule notes
+//   nextNoteTime: 0, 
+//   currentNote: 0, 
+//   syllableSamples: {},
+//   numSyllables: 0, 
+//   // currentEventListeners: [],
+// };
+
+document.addEventListener("DOMContentLoaded", init); 
 
 
 // should maybe change this to a debouncing function that fires after a certain amount of time elapses without typing
 document.querySelector("#input-text-form").addEventListener("submit", handleInput);
+
+
+// GLOBAL CONTROLS
+const tempoControl = document.querySelector('#tempo'); 
+tempoControl.addEventListener('input', (e) => {
+  currentStateObj.tempo = Number(e.currentTarget.value); 
+})
+
+function init() {
+  meSpeak.loadVoice('en/en-us'); 
+}
 
 async function handleInput(e) {
   e.preventDefault(); 
   resetTracks(); 
   
   const inputText = e.currentTarget.firstElementChild.firstElementChild.value; 
-  // const ipa = await getWordIPA(inputText); 
+  currentStateObj.currentInput = inputText; 
+
+  // RESET THE INPUT
+  e.currentTarget.firstElementChild.firstElementChild.innerText = ""; 
   
-  // const syllables = await getWordSyllables(inputText); 
-  debugger
+
+  // CREATE THE TRACKS
+  const trackContainer = document.getElementById('track-wrapper');
+  const syllables = await getWordSyllables(inputText); 
+  currentStateObj.numSyllables = syllables.length; 
+  setupTracks(syllables, trackContainer); 
   
+  // INITIALIZE AUDIO_CONTEXT
+  const ctx = new AudioContext(); 
+  
+
   // FOR TESTING
-  const audio = loadSyllableSound('hel'); 
-  // const audio = loadSyllableSound(syllables[0]); 
+  // const audio = await loadSyllableSound('hel', ctx); 
+  // await loadSyllableSound('hel', ctx).then( res => {
+  //   debugger
+  //   currentStateObj.syllableSamples[0] = res
+  // }); 
+  // currentStateObj.syllableSamples[0] = audio;  
 
-  debugger
-  // const ipaSyllables = syllables.map(syl => syllableToIpa(syl) );
-  // console.log(ipaSyllables);
-
-
-  // playWord(inputText); 
-
-  // setupTracks(syllables, document.getElementById('track-wrapper')); 
-  // playIPA(syllables[0]); 
-
+  
+  for (let i = 0; i < syllables.length; i++) {
+    loadSyllableSound(syllables[i], ctx, i); 
+  }
 }
 
 
@@ -50,4 +86,8 @@ function unregisterEventListeners() {
 
 function resetTracks() {
   console.log("in reset tracks");
+
+  // should remove all event listeners and trigger a remove all tracks
+
+  // removeTracks(document.getElementById('track-wrapper')); 
 }
--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -1,41 +1,24 @@
-// // create synthesizer that will use speak()
-// const synth = window.speechSynthesis; 
-
-// // choose a voice
-// const voices = synth.getVoices(); 
-
-// // choose first US english by default for now
-// const chosenVoice = voices.filter(vox => vox.lang === 'en-US')[0]; 
-
-// // create a new utterance with inputted text
-// const utterance = new SpeechSynthesisUtterance("default"/* TEXT FROM THE INPUT */ );
-
-// utterance.voice = chosenVoice; 
-
-// setInterval(synth.speak(utterance), 1000); 
 
 
+export const currentStateObj = {
+  audioContext: null, 
+  isPlaying: false, 
+  startTime: null, 
+  current16thNote: null,     // last note that was scheduled to be played
+  tempo: 50.0,              // measured in bpm
+  lookAhead: 25.0,          // how frequently to call the scheduling function
+  scheduleAheadTime: 0.1,   // how far ahead to schedule notes
+  timeToNextNote: 0, 
+  currentNote: 0, 
+  syllableSamples: {},
+  numSyllables: 0, 
+  noteQueue: [],
+  // currentEventListeners: [],
+};
 
-export const playWord = (inputText) => {
-  const synth = window.speechSynthesis; 
-  const voices = synth.getVoices(); 
-  const chosenVoice = voices.filter(vox => vox.lang === 'en-US')[0]; 
-  const utterance = new SpeechSynthesisUtterance(inputText);
-  
-  utterance.voice = chosenVoice; 
-  
-  synth.speak(utterance); 
-}
-
-export const playAudio = (audio) => {
-
-}
-
-
+// ME_SPEAK
 export const playIPA = (ipa) => {
-  debugger
-
-  
+  debugger  
   meSpeak.loadVoice('en/en-us');
   meSpeak.speak(ipa); 
   
@@ -46,35 +29,135 @@ export const playIPA = (ipa) => {
 
   // meSpeak.play(spoken);
 }
-export async function loadSyllableSound(syllable, audioContext) {
-  meSpeak.loadVoice('en/en-us', async () => {
-    await meSpeak.speak(syllable, {rawdata: true}, async (success, id, stream) => {
-      if (success) {
-        return await getAudioBuffer(stream, audioContext); 
-      }
-    });  
-  });
-}
 
+export async function loadSyllableSound(syllable, audioContext, trackIdx) {
+  currentStateObj.audioContext  ||= audioContext; 
+  let audio; 
+
+  await meSpeak.speak(syllable, {rawdata: true}, async (success, id, stream) => {
+    if (success) {
+      debugger
+      audio = await getAudioBuffer(stream, audioContext); 
+      currentStateObj.syllableSamples[trackIdx] = audio; 
+    }
+  });  
+}
 function getAudioBuffer(data, audioContext) {
   return audioContext.decodeAudioData(data); 
 }
 
 
 
-export const handleClick = (e) => {
-  console.log("you're in the handleClick method");
+export const handleClick = (e) => { 
+  const button = e.target; 
 
-  // turns some boolean to true so the node can be played
-  debugger // what is e? 
+  // SET THE CLASS FOR STYLING AND DATA FOR PLAYBACK
+  button.classList.toggle('active');   
+  button.dataset.active = button.dataset.active === "true" ? false : true; 
 }
 
-export const play = () => {
-
+export const playSyllable = (audioBuffer, time) => {
+  const ctx = currentStateObj.audioContext; 
 
   const audioSource = ctx.createBufferSource();
   audioSource.connect( ctx.destination );
 
-  audioSource.buffer = //audio buffer
-  audioSource.start(); 
+  audioSource.buffer = audioBuffer; 
+  audioSource.start(time); 
+  return audioSource; 
+}
+
+function nextNote() {
+  const secondsPerBeat = 60.0 / currentStateObj.tempo; 
+
+  currentStateObj.timeToNextNote += secondsPerBeat; 
+  currentStateObj.currentNote++; 
+
+  // THIS IS WHAT LIMITS THE NUM OF BEATS FOR THE AUDIO
+  if (currentStateObj.currentNote === 8) {
+    currentStateObj.currentNote = 0; 
+  }
+}
+
+function scheduleNotes(beatNum, time){
+  const tracks = document.querySelectorAll('track'); 
+
+  currentStateObj.noteQueue.push({ note: beatNum, time }); 
+
+  for (let i = 0; i < tracks.length; i++) {
+
+    debugger // find the correct track and button at the right beatnum
+    if (tracks[i]) {  
+      const buffer = currentStateObj.syllableSamples[i]; 
+      
+      playSyllable(buffer, time)
+    }
+  }
+
+
+}
+
+// from MDN docs on web audio api advanced techniques and Chris Wilson's A Tale Of Two Clocks article which MDN references
+function scheduler() {
+  const { 
+    timeToNextNote,
+    scheduleAheadTime,
+    audioContext,
+    currentNote
+  } = currentStateObj; 
+
+  while (timeToNextNote < audioContext.currentTime + scheduleAheadTime ) {
+      scheduleNotes(currentNote, timeToNextNote);
+      nextNote();
+  }
+  timerID = window.setTimeout(scheduler, lookahead);
+}
+
+window.scheduler = scheduler; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SPEECH_SYNTHESIS
+{
+  // // create synthesizer that will use speak()
+  // const synth = window.speechSynthesis; 
+  
+  // // choose a voice
+  // const voices = synth.getVoices(); 
+  
+  // // choose first US english by default for now
+  // const chosenVoice = voices.filter(vox => vox.lang === 'en-US')[0]; 
+  
+  // // create a new utterance with inputted text
+  // const utterance = new SpeechSynthesisUtterance("default"/* TEXT FROM THE INPUT */ );
+  
+  // utterance.voice = chosenVoice; 
+  
+  // setInterval(synth.speak(utterance), 1000); 
+}
+
+export const playWord = (inputText) => {
+  const synth = window.speechSynthesis; 
+  const voices = synth.getVoices(); 
+  const chosenVoice = voices.filter(vox => vox.lang === 'en-US')[0]; 
+  const utterance = new SpeechSynthesisUtterance(inputText);
+  
+  utterance.voice = chosenVoice; 
+  
+  synth.speak(utterance); 
 }
