4217471dbd3d08a3755c3c70f496178b7a912709
fdiengott <freddiengott@gmail.com>
2021-06-02 16:06:01 -0400

    Update track creator methods to attach data attributes

--- src/scripts/track.js
+++ src/scripts/track.js
@@ -1,19 +1,21 @@
 
+import { handleClick } from './audio';
+
 
 export const setupTracks = (syllables, parentNode) => {
   console.log('inside setupTracks');
   
-  syllables.forEach(syl => {
-    parentNode.appendChild(trackTemplate(syl, null))
+  syllables.forEach((syl, i) => {
+    parentNode.appendChild(trackTemplate(syl, i))
   });
 }
 
-const trackTemplate = (displayText, audio) => {
+const trackTemplate = (displayText, idx) => {
   // this will create the DOM elements that every track needs without the specifics
 
   // this is the container for each track
   const section = document.createElement('section');
-  section.classList = 'track'; 
+  section.classList = 'track-section'; 
 
   const aside = document.createElement('aside'); 
   const h3 = document.createElement('h3'); 
@@ -23,15 +25,22 @@ const trackTemplate = (displayText, audio) => {
   section.appendChild(aside); 
 
   const buttonDiv = document.createElement('div'); 
+  buttonDiv.classList = `track`; 
+  buttonDiv.classList = `track-${idx}`; 
 
   for (let i = 0; i < 8; i++) {
     let button = document.createElement('button'); 
 
-    // probably need to add a specific event listener
-
+    
     button.classList = `beat-btn btn-${i}`; 
+    button.dataset.beatNum = i; 
+    button.dataset.active = false; 
     buttonDiv.appendChild(button); 
   }
+  
+  // probably need to add a specific event listener    
+  // buttonDiv.addEventListener("click", handleClick, { capture: false })
+  buttonDiv.addEventListener("click", handleClick, false )
 
   section.appendChild(buttonDiv); 
 
@@ -41,6 +50,13 @@ const trackTemplate = (displayText, audio) => {
 
 }
 
-export const removeTracks = () => {
+export const removeTracks = (parentNode) => {
+
+  // MUST ALSO REMOVE ALL EVENT LISTENERS
+
+  while (parentNode.firstChild()) {
+    parentNode.removeChild(parentNode.firstChild)
+  }
 
 }
+
